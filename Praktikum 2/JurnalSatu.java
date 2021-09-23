4216fa2f11a463d382a67d73e09b37a715213b93
unknown <irsallazuard547@gmail.com>
2021-09-23 18:55:01 +0700

    JurnalSatu.java

---
+++ Praktikum 2/JurnalSatu.java
@@ -0,0 +1,9 @@
+public class JurnalSatu {
+	
+	public static void main (String[] args) {
+		System.out.println("Nama: Irsal Lazuard Izzatillah");
+		System.out.println("NIM: 6706213006");
+		System.out.println("Kelas: 45-05");
+	}
+}	
+		
